6009597aaf04104cb9daf8b5760c556f06168648
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-26 19:35:12 +0800

    convert json string to json object added check if conversion was successful

--- src/index.php
+++ src/index.php
@@ -1,4 +1,15 @@
 <?php
 
+// retrieve json string from upper api
 $source = file_get_contents('http://weather.asti.dost.gov.ph/web-api/index.php/api/data/1520');
-echo $source;
+
+// convert json string to json object
+$json = json_decode($source);
+if (is_null($json)) {
+  echo '\'DATA_ERROR\'';
+  exit;
+}
+
+echo '<pre>';
+print_r($json);
+echo '</pre>';
